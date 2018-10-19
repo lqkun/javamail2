38ab09f61c64eabf153cc6f2a48a79e1fb122b81
lqkun <1129706754@qq.com>
2018-10-19 15:43:45 +0800

    javaemail

--- src/cn/itcast/javamail2/Base64Util.java
+++ src/cn/itcast/javamail2/Base64Util.java
@@ -1,35 +1,8 @@
 package cn.itcast.javamail2;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.Properties;
-
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
-import javax.mail.Address;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.Message.RecipientType;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.internet.MimeUtility;
-
-
-
-
 import sun.misc.BASE64Encoder;
 
 public class Base64Util {
--- src/cn/itcast/javamail2/util/SendEmail.java
+++ src/cn/itcast/javamail2/util/SendEmail.java
@@ -9,6 +9,13 @@ import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.*;
  
+/**  
+*  <p>Title: SendEmail.java</p>
+*  <p>Package: cn.itcast.javamail2.util</p>
+*  <p>Description:(邮件发送util)</p>
+* @author: Lqkun
+* @date:   2018年10月19日 下午3:40:24 
+*/
 public class SendEmail {
  
     public interface Callback {
@@ -305,5 +312,37 @@ public class SendEmail {
         session = Session.getInstance(properties);
         session.setDebug(debug);
     }
- 
+    //测试邮件发送
+    public static void main(String[] args) throws Exception {
+		 //gutwnqnuqgfqbedf
+	        List<String> map = new ArrayList<>();
+	      /*  map.add("123456@qq.com");
+	        map.add("456789@qq.com");
+	        map.add("hahaha123@gmail.com");*/
+	        new SendEmail("653591018@qq.com", "gutwnqnuqgfqbedf")
+	                .setDebug(true)
+	                .setMyNickName("这是我的昵称")
+	                .addFile("D://1.jpg")//添加附件
+	                .addFile("D://QuanSongCi.txt")
+//	              .addFile(List<String> list)//添加附件集合
+	              .setSaveEmail("D://name.eml")//保存邮件
+	              .addRecipientT0("653591018@qq.com")//添加收件人地址
+//	            .addRecipientT0(map)//添加收件人地址集合
+//	            .addRecipientCC(map)//添加密送收件人地址
+//	            .addRecipientBCC(map)//添加抄送收件人地址
+	              .createMail("标题", "发送的内容", "text/html;charset=utf-8")
+	              .sendEmail(new SendEmail.Callback() {
+	                    @Override
+	                    public void success(String s) {
+	                        System.out.println(s);//发送完成后回调接口
+	                    }
+	 
+	                    @Override
+	                    public void error(String s, Exception e) {
+	                        System.out.println(s);
+	                        e.printStackTrace();//异常失败的回调接口
+	                    }
+	                });
+	    }
+
 }
